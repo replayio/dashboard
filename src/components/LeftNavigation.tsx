f50cda72465397681a1dad58139cda1e5311c76e
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-03-08 15:52:18 -0500

    Made left nav work

--- src/components/LeftNavigation.tsx
+++ src/components/LeftNavigation.tsx
@@ -1,5 +1,7 @@
-import { getNonPendingWorkspaces } from "@/graphql/queries/getNonPendingWorkspaces";
+import { LeftNavigationLink } from "@/components/LeftNavigationLink";
 import { getCurrentUser } from "@/graphql/queries/getCurrentUser";
+import { getNonPendingWorkspaces } from "@/graphql/queries/getNonPendingWorkspaces";
+import Image from "next/image";
 
 // TODO Highlight selected team
 export async function LeftNavigation() {
@@ -7,14 +9,24 @@ export async function LeftNavigation() {
   const user = await getCurrentUser();
 
   return (
-    <nav className="flex flex-col h-full px-2 py-1 bg-slate-800 text-white overflow-auto shrink-0">
-      {workspaces.map(({ id, isTest, name }) => (
-        <div key={id}>
-          {isTest} {name}
-        </div>
-      ))}
+    <nav className="flex flex-col h-full bg-slate-800 text-white overflow-auto shrink-0">
+      <div className="p-4">
+        <Image
+          alt="Replay logo"
+          className="h-8 w-8"
+          height={32}
+          src="/images/replay-logo.svg"
+          width={32}
+        />
+      </div>
+      <div className="flex flex-col overflow-auto">
+        <LeftNavigationLink id="me" isTest={false} name="Your Library" />
+        {workspaces.map(({ id, isTest, name }) => (
+          <LeftNavigationLink id={id} isTest={isTest} key={id} name={name} />
+        ))}
+      </div>
       <div className="grow" />
-      <div className="flex flex-row items-center gap-2">
+      <div className="flex flex-row items-center gap-2 bg-slate-700 px-2 py-1">
         {user.picture && (
           <img src={user.picture} className="rounded-full w-8 h-8" />
         )}
