5d8437e23b7323e7a41755f185a29d3abb3cb468
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-03-09 19:09:04 -0500

    Add coming-soon message for Test Suites workspaces

--- src/app/team/[id]/recordings/page.tsx
+++ src/app/team/[id]/recordings/page.tsx
@@ -3,8 +3,11 @@ import { Recording } from "@/app/team/[id]/recordings/Recording";
 import { ShowMoreRecordingsRow } from "@/app/team/[id]/recordings/ShowMoreRecordingsRow";
 import { PAGE_SIZE } from "@/app/team/[id]/recordings/shared";
 import { Button } from "@/components/Button";
+import { getNonPendingWorkspacesServer } from "@/graphql/queries/getNonPendingWorkspaces";
 import { getPersonalRecordingsServer } from "@/graphql/queries/getPersonalRecordings";
 import { getWorkspaceRecordingsServer } from "@/graphql/queries/getWorkspaceRecordings";
+import { WorkspaceRecording } from "@/graphql/types";
+import assert from "assert";
 
 export default async function Page({
   params,
@@ -18,15 +21,29 @@ export default async function Page({
 }) {
   const id = decodeURIComponent(params.id);
 
-  const limit = searchParams.limit ?? PAGE_SIZE;
-  const filter = searchParams.filter ?? "";
+  let isTestWorkspace = false;
+  if (id !== "me") {
+    const workspaces = await getNonPendingWorkspacesServer();
+    const workspace = workspaces.find((workspace) => workspace.id === id);
+    assert(workspace, `Workspace "${id}" not found`);
 
-  const recordings =
-    id === "me"
-      ? await getPersonalRecordingsServer(filter)
-      : await getWorkspaceRecordingsServer(id, filter);
+    isTestWorkspace = workspace.isTest;
+  }
 
-  const recordingToDisplay = recordings.slice(0, limit);
+  let recordings: WorkspaceRecording[];
+  if (isTestWorkspace) {
+    recordings = [];
+  } else {
+    const limit = searchParams.limit ?? PAGE_SIZE;
+    const filter = searchParams.filter ?? "";
+
+    const allRecordings =
+      id === "me"
+        ? await getPersonalRecordingsServer(filter)
+        : await getWorkspaceRecordingsServer(id, filter);
+
+    recordings = allRecordings.slice(0, limit);
+  }
 
   return (
     <div className="flex flex-col gap-4 overflow-auto overflow-hidden p-4">
@@ -35,12 +52,18 @@ export default async function Page({
         <Button>Launch Replay</Button>
       </div>
       <div className="overflow-auto flex flex-col gap-2">
-        <div className="overflow-auto bg-slate-900 text-white rounded flex flex-col gap-px">
-          {recordingToDisplay.map((recording) => (
-            <Recording key={recording.uuid} recording={recording} />
-          ))}
-          <ShowMoreRecordingsRow maxLimit={recordings.length} />
-        </div>
+        {isTestWorkspace ? (
+          <div className="bg-amber-800 text-amber-200 p-2 rounded">
+            <strong>Coming soon</strong>: Test suites
+          </div>
+        ) : (
+          <div className="overflow-auto bg-slate-900 text-white rounded flex flex-col gap-px">
+            {recordings.map((recording) => (
+              <Recording key={recording.uuid} recording={recording} />
+            ))}
+            <ShowMoreRecordingsRow maxLimit={recordings.length} />
+          </div>
+        )}
       </div>
     </div>
   );
