d83773e8eaa3ec5648b179ea7188db756f86c9bb
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-03-12 12:00:29 -0400

    Show test suite recording time

--- src/app/team/[id]/recordings/TestSuite/RecordingRow.tsx
+++ src/app/team/[id]/recordings/TestSuite/RecordingRow.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@/components/Icon";
 import { TestSuiteTestRecording, TestSuiteTestStatus } from "@/graphql/types";
+import { formatRelativeTime } from "@/utils/number";
 import { getURL } from "@/utils/recording";
 import { getColorClassName } from "@/utils/test-suites";
 import Link from "next/link";
@@ -25,6 +26,10 @@ export function RecordingRow({
     >
       <Icon className={`w-6 h-6 shrink-0 ${colorClassName}`} type={iconType} />
       <div className="truncate shrink">View recording</div>
+      <div className="flex flex-row gap-1 items-center shrink-0 text-sm text-slate-300">
+        <Icon className="w-4 h-4" type="clock" />
+        {formatRelativeTime(recording.createdAt)}
+      </div>
     </Link>
   );
 }
