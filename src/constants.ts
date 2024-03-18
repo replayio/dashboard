af58415328f80564b9adc8debac750e96218ad1f
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-03-18 09:13:29 -0400

    Remove unused constants

--- src/constants.ts
+++ src/constants.ts
@@ -10,9 +10,3 @@ export const LOCAL_STORAGE = {
   testFilters: "replay:dashboard:tests-filters",
   testRunsFilters: "replay:dashboard:test-runs-filters",
 };
-
-export const HOST =
-  process.env.NEXT_PUBLIC_APP_URL ??
-  `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-
-export const AUTH_REDIRECT_URI = HOST;
