849b709d0cca4bae571631569b712b9dc6c6a745
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-03-20 09:34:33 -0400

    Workaround for bad Apollo TestRun caching behavior

--- src/graphql/graphQLClient.ts
+++ src/graphql/graphQLClient.ts
@@ -2,8 +2,11 @@ import {
   ApolloClient,
   InMemoryCache,
   NormalizedCacheObject,
+  createHttpLink,
+  from,
 } from "@apollo/client";
 import { loadDevMessages, loadErrorMessageHandler } from "@apollo/client/dev";
+import { RetryLink } from "@apollo/client/link/retry";
 
 if (process.env.NODE_ENV === "development") {
   loadDevMessages();
@@ -14,6 +17,24 @@ let graphQLClient: ApolloClient<NormalizedCacheObject>;
 
 export function getGraphQLClient(accessToken: string) {
   if (graphQLClient == null) {
+    const httpLink = createHttpLink({
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+        "Replay-Client-Id": "196a9e7b-dba5-46ee-8b81-fac66991f431",
+      },
+      uri: "https://api.replay.io/v1/graphql",
+    });
+    const retryLink = new RetryLink({
+      attempts: {
+        retryIf: (error) => {
+          // GraphQL errors appear in error?.result?.errors.
+          // We don't want to retry requests that failed due to GraphQL errors.
+          return !error?.result?.errors?.length;
+        },
+      },
+    });
+
     graphQLClient = new ApolloClient({
       cache: new InMemoryCache({
         typePolicies: {
@@ -42,6 +63,23 @@ export function getGraphQLClient(accessToken: string) {
               },
             },
           },
+          // Apollo's default merge behavior incorrectly normalizes TestRunTest objects instead of scoping them to the parent TestRun
+          // The result is that executions data gets overridden when a new TestRun is added to the cache
+          // The custom merge function below prevents this from happening but at the cost of breaking in-memory caching TestRun data
+          // See FE-2367
+          TestRunTest: {
+            fields: {
+              executions: {
+                merge(
+                  existing: any[],
+                  incoming: any[],
+                  { readField, mergeObjects }
+                ) {
+                  // No-op
+                },
+              },
+            },
+          },
         },
       }),
       defaultOptions: {
@@ -49,12 +87,7 @@ export function getGraphQLClient(accessToken: string) {
           fetchPolicy: "cache-first",
         },
       },
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
-        "Replay-Client-Id": "196a9e7b-dba5-46ee-8b81-fac66991f431",
-      },
-      uri: "https://api.replay.io/v1/graphql",
+      link: from([httpLink, retryLink]),
     });
   }
 
