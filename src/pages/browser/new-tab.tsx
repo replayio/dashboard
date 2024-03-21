28423a0a5bd38560b6618bf6e5f8b3aba6fcc178
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-03-20 21:22:39 -0400

    Add browser/new-tab route

---
+++ src/pages/browser/new-tab.tsx
@@ -0,0 +1,17 @@
+import { EmptyLayout } from "@/components/EmptyLayout";
+import { Hoverboard } from "@replayio/overboard";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-4 items-center text-center">
+      <div className="w-36 h-36">
+        <Hoverboard />
+      </div>
+      Please navigate to the page you want to record,
+      <br />
+      then press the blue record button.
+    </div>
+  );
+}
+
+Page.Layout = EmptyLayout;
