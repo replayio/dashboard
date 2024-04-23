f476615cf464fc9ca77184c57e87fb0f564c026b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-04-23 11:23:49 +0200

    Redirect `/team/[id]` to appropriate dashboard route

---
+++ src/pages/team/[id]/index.tsx
@@ -0,0 +1,33 @@
+import { getServerSidePropsHelpers as getServerSidePropsShared } from "@/pageComponents/team/id/getServerSidePropsHelpers";
+import { redirectWithState } from "@/utils/redirectWithState";
+import { GetServerSidePropsContext } from "next";
+
+export default function Page() {
+  return null;
+}
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<any>
+) {
+  const { invalidWorkspace, isTest, workspaceId } =
+    await getServerSidePropsShared(context);
+
+  if (invalidWorkspace) {
+    return redirectWithState({
+      context,
+      pathname: "/team/me/recordings",
+      props: {
+        workspaceId,
+      },
+    });
+  }
+  return redirectWithState({
+    context,
+    pathname: isTest
+      ? `/team/${workspaceId}/runs`
+      : `/team/${workspaceId}/recordings`,
+    props: {
+      workspaceId,
+    },
+  });
+}
