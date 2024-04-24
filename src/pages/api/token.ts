9286d1366e7b7757531065f479a6c20c3dd9ac5a
Holger Benl <hbenl@evandor.de>
2024-04-24 14:33:30 +0200

    Use the access token cookie in the token endpoint

--- src/pages/api/token.ts
+++ src/pages/api/token.ts
@@ -1,3 +1,5 @@
+import { COOKIES } from "@/constants";
+import { AccessTokenCookie } from "@/utils/cookie";
 import { getAccessToken } from "@auth0/nextjs-auth0";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -6,6 +8,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { accessToken } = await getAccessToken(req, res);
     res.status(200).send(accessToken);
   } catch {
+    // An active auth0 session should always take precedence
+    const accessTokenCookie = req.cookies[COOKIES.accessToken];
+    if (accessTokenCookie) {
+      const tokenWithSource: AccessTokenCookie = JSON.parse(accessTokenCookie);
+      if (typeof tokenWithSource === "object" && tokenWithSource.token) {
+        res.status(200).send(tokenWithSource.token);
+        return;
+      }
+    }
+
     res.status(401).send("Not logged in");
   }
 }
