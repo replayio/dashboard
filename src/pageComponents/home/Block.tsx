c385ad7cfd33e503fa2fff258b15e3b2226aa47e
Jon Bell <github@lot23.com>
2024-05-22 15:05:28 +1200

    CSS adjustments

--- src/pageComponents/home/Block.tsx
+++ src/pageComponents/home/Block.tsx
@@ -1,6 +1,7 @@
 import { ExternalLink } from "@/components/ExternalLink";
 import Link from "next/link";
 import { PropsWithChildren } from "react";
+import styles from './Block.module.css';
 
 export function Block({
   children,
@@ -11,12 +12,12 @@ export function Block({
 
   return (
     <Component
-      className="rounded-md flex flex-col bg-slate-700 overflow-hidden w-72 hover:bg-slate-600 cursor-pointer transition text-white"
+      className={styles.block}
       href={href}
     >
-      <div className="bg-gradient-to-br from-sky-500 to-pink-300 w-full h-32" />
-      <div className="flex flex-col flex-wrap gap-2 p-6">
-        <div className="text-xl">{title}</div>
+      <div className={styles.gradient} />
+      <div className={styles.content}>
+        <div className={styles.title}>{title}</div>
         <div>{children}</div>
       </div>
     </Component>
