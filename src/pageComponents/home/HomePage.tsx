d6be65df768f30ef4e11fcd735791ad47578a1d5
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-05-22 16:02:31 -0400

    Don't use black background. (It's not a standard Dashboard color.)

--- src/pageComponents/home/HomePage.tsx
+++ src/pageComponents/home/HomePage.tsx
@@ -3,14 +3,12 @@ import { Block } from "@/pageComponents/home/Block";
 export function HomePage() {
   return (
     <div className="flex items-center justify-center h-full p-2 overflow-auto">
-      <div className="flex flex-col gap-4 p-6 pt-2 bg-none shadow-lg w-[827px] rounded-xl lg:bg-black">
+      <div className="flex flex-col gap-4 p-6 pt-2 bg-none shadow-lg w-[827px] rounded-xl lg:bg-slate-950">
         <div className="pt-8 text-3xl font-medium lg:pt-2">Home</div>
         <div className="flex flex-row flex-wrap gap-4">
           <Block href="https://docs.replay.io/quickstart" title="Quickstart">
             Record your first replay with{" "}
-            <code className="p-1 text-sm text-white rounded bg-slate-800">
-              npx replayio record
-            </code>
+            <code className="p-1 text-sm text-white rounded bg-slate-800">npx replayio record</code>
           </Block>
           <Block href="/team/new/standard" title="Add a new team">
             Set up a new team to store your replays in one place
