b98703834e30b8dde0a5a4af090988d866f0690b
Mark Erikson <mark@isquaredsoftware.com>
2024-05-30 14:54:42 -0500

    Fill out category mutations

--- src/graphql/queries/useRootCauseCategoryMutations.ts
+++ src/graphql/queries/useRootCauseCategoryMutations.ts
@@ -18,6 +18,29 @@ interface CreateRootCauseCategoryMutationVariables {
   name: string;
 }
 
+interface UpdateRootCauseCategoryMutation {
+  updateRootCauseCategory: {
+    category: RCACategory;
+  };
+}
+
+interface UpdateRootCauseCategoryMutationVariables {
+  workspaceId: string;
+  categoryId: string;
+  name: string;
+}
+
+interface DeleteRootCauseCategoryMutation {
+  deleteRootCauseCategory: {
+    success: boolean;
+  };
+}
+
+interface DeleteRootCauseCategoryMutationVariables {
+  workspaceId: string;
+  categoryId: string;
+}
+
 export function decodeEncodedId(encodedId: string) {
   return atob(encodedId).split(":")[1]!;
 }
@@ -54,4 +77,75 @@ export function useCreateRootCauseCategory() {
   };
 
   return { createRootCauseCategory, error, isLoading };
+}
+
+export function useUpdateRootCauseCategory() {
+  const {
+    error,
+    isLoading,
+    mutate: updateRootCauseCategoryMutation,
+  } = useGraphQLMutation<UpdateRootCauseCategoryMutation, UpdateRootCauseCategoryMutationVariables>(
+    gql`
+      mutation UpdateRootCauseCategory(
+        $workspaceId: String!
+        $categoryId: String!
+        $name: String!
+      ) {
+        updateRootCauseCategory(
+          input: { workspaceId: $workspaceId, categoryId: $categoryId, name: $name }
+        ) {
+          category {
+            id
+            name
+          }
+        }
+      }
+    `,
+    {
+      refetchQueries: ["GetWorkspaceRootCauseCategories"],
+    }
+  );
+
+  const updateRootCauseCategory = (workspaceId: string, categoryId: string, name: string) => {
+    // Our workspaceId is base64 encoded, so we need to decode it before sending it to the backend.
+    // This should probably be handled more consistently somehow.
+    const decodedWorkspaceId = decodeEncodedId(workspaceId);
+
+    return updateRootCauseCategoryMutation({
+      variables: { workspaceId: decodedWorkspaceId, categoryId, name },
+    });
+  };
+
+  return { updateRootCauseCategory, error, isLoading };
+}
+
+export function useDeleteRootCauseCategory() {
+  const {
+    error,
+    isLoading,
+    mutate: deleteRootCauseCategoryMutation,
+  } = useGraphQLMutation<DeleteRootCauseCategoryMutation, DeleteRootCauseCategoryMutationVariables>(
+    gql`
+      mutation DeleteRootCauseCategory($workspaceId: String!, $categoryId: String!) {
+        deleteRootCauseCategory(input: { workspaceId: $workspaceId, categoryId: $categoryId }) {
+          success
+        }
+      }
+    `,
+    {
+      refetchQueries: ["GetWorkspaceRootCauseCategories"],
+    }
+  );
+
+  const deleteRootCauseCategory = (workspaceId: string, categoryId: string) => {
+    // Our workspaceId is base64 encoded, so we need to decode it before sending it to the backend.
+    // This should probably be handled more consistently somehow.
+    const decodedWorkspaceId = decodeEncodedId(workspaceId);
+
+    return deleteRootCauseCategoryMutation({
+      variables: { workspaceId: decodedWorkspaceId, categoryId },
+    });
+  };
+
+  return { deleteRootCauseCategory, error, isLoading };
 }
