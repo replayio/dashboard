69740a8eb06d19721252d10b2074246661a22ae5
Jon Bell <github@lot23.com>
2024-06-07 12:52:44 +1200

    Refined dashboard design (#140)
    * Initial commit
    
    * Cleaning up left nav
    
    * Left nav updates
    
    * Fixing links
    
    * Update src/components/LeftNav.tsx
    
    Co-authored-by: Brian Vaughn <brian.david.vaughn@gmail.com>
    
    * Update src/components/LeftNav.tsx
    
    Co-authored-by: Brian Vaughn <brian.david.vaughn@gmail.com>
    
    * Update src/pageComponents/home/Block.tsx
    
    Co-authored-by: Brian Vaughn <brian.david.vaughn@gmail.com>
    
    * Addressing bvaughn feedback
    
    * Removing old import
    
    * Suggested changes (code only, no visual) (#143)
    
    - Make Block components responsive
    - Inline `CreateTestSuiteLink` and `NewTeamLink` components
    - Updated `CurrentUser` renderer (fix flex shrink, fix padding, no rounded corners)
    - Fix failing e2e test
    
    A note about the `CurrentUser` changes
    
    | before | after |
    | :--- | :--- |
    | ![Screenshot 2024-06-06 at 7 35 30 AM](https://github.com/replayio/dashboard/assets/29597/c8f0b1b0-89b1-4fb2-b0c4-85051e49e2bb) | ![Screenshot 2024-06-06 at 7 34 42 AM](https://github.com/replayio/dashboard/assets/29597/53498290-e00f-4114-a483-c6f83d05a2a0) |
    | ![Screenshot 2024-06-06 at 7 35 41 AM](https://github.com/replayio/dashboard/assets/29597/7841851c-3b33-4c4c-bdc0-1bf9c6c8f360) | ![Screenshot 2024-06-06 at 7 34 35 AM](https://github.com/replayio/dashboard/assets/29597/1955bbba-9f66-4775-a32a-05ed1e08f511) |
    
    * Smaller images
    
    ---------
    
    Co-authored-by: Brian Vaughn <brian.david.vaughn@gmail.com>

--- src/pageComponents/home/Block.tsx
+++ src/pageComponents/home/Block.tsx
@@ -5,18 +5,23 @@ import { PropsWithChildren } from "react";
 export function Block({
   children,
   href,
+  imageUrl,
   title,
-}: PropsWithChildren<{ href: string; title: string }>) {
+}: PropsWithChildren<{ href: string; imageUrl: string; title: string }>) {
   const Component = href.startsWith("/") || href.startsWith("mailto:") ? Link : ExternalLink;
 
   return (
     <Component
-      className="flex flex-col w-64 overflow-hidden text-white transition rounded-md cursor-pointer bg-slate-700 hover:bg-slate-600"
+      className="flex flex-col overflow-hidden text-white transition rounded-md lg:shadow-xl cursor-pointer lg:w-96 lg:bg-slate-950 lg:hover:bg-black"
       href={href}
     >
-      <div className="hidden w-full h-32 bg-gradient-to-br from-sky-500 to-pink-300 lg:block short:hidden" />
-      <div className="flex flex-col flex-wrap gap-2 p-6">
-        <div className="text-xl">{title}</div>
+      <div
+        className="w-full h-32 bg-center bg-cover short:hidden hidden lg:block"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+
+      <div className="flex flex-col flex-wrap lg:gap-2 lg:p-6">
+        <div className="text-xl text-white underline lg:no-underline">{title}</div>
         <div>{children}</div>
       </div>
     </Component>
--- src/pageComponents/home/HomePage.tsx
+++ src/pageComponents/home/HomePage.tsx
@@ -1,28 +1,34 @@
 import { Block } from "@/pageComponents/home/Block";
+import blogImage from "@/pageComponents/home/blog.png";
+import courseImage from "@/pageComponents/home/course.png";
+import documentationImage from "@/pageComponents/home/documentation.png";
+import supportImage from "@/pageComponents/home/support.png";
 
 export function HomePage() {
   return (
-    <div className="flex items-start justify-start h-full p-2 overflow-auto lg:justify-center lg:items-center">
-      <div className="grid grid-cols-1 gap-2 shadow-lg md:grid-cols-2 lg:grid-cols-3 bg-none rounded-xl lg:bg-slate-950 lg:p-6 lg:gap-4">
-        <div className="text-2xl font-medium col-span-full md:col-span-2 lg:col-span-3">Home</div>
-        <Block href="https://docs.replay.io/quickstart" title="Quickstart">
-          Record your first replay with{" "}
-          <code className="p-1 text-sm text-white rounded bg-slate-800">npx replayio record</code>
+    <div className="flex items-start justify-start h-full p-6 overflow-auto lg:justify-center lg:items-center">
+      <div className="flex flex-col gap-2 lg:grid lg:grid-cols-2 lg:p-6 lg:gap-6 lg:shadow-lg lg:border border-slate-700 bg-none rounded-xl lg:bg-slate-800">
+        <div className="text-2xl font-medium col-span-full hidden lg:block">Home</div>
+
+        <Block
+          href="https://docs.replay.io/basics/getting-started/record-your-app"
+          title="Documentation"
+          imageUrl={documentationImage.src}
+        >
+          Filled with examples to help you on your way
         </Block>
-        <Block href="/team/new/standard" title="Add a new team">
-          Set up a new team to store your replays in one place
-        </Block>
-        <Block href="/team/new/tests" title="Create Test Suite">
-          Use Replay to record Playwright or Cypress tests
+        <Block href="https://www.replay.io/support" title="Support" imageUrl={supportImage.src}>
+          Contact us on Discord, we&apos;re here to help!
         </Block>
         <Block
           href="https://www.youtube.com/playlist?list=PLOHkr6ZaQDlH5M0PZzjVG9ohyqw7avwPa"
           title="Replay Course"
+          imageUrl={courseImage.src}
         >
-          Our class on YouTube is great for learning the basics
+          Our course on YouTube is great for learning the basics
         </Block>
-        <Block href="https://www.replay.io/contact" title="Support">
-          Contact us on Discord or send us a note
+        <Block href="https://blog.replay.io" title="Replay Blog" imageUrl={blogImage.src}>
+          See how other teams perform time travel miracles with Replay
         </Block>
       </div>
     </div>
