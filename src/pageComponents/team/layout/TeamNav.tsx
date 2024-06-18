52b8b1e085b7c6cdffeb73a9b1c4a3d78b44f958
Aditya Pandey <aditya@autospace.co>
2024-06-17 19:10:21 -0500

    Temporarily disable tests view

--- src/pageComponents/team/layout/TeamNav.tsx
+++ src/pageComponents/team/layout/TeamNav.tsx
@@ -1,14 +1,20 @@
+import { Icon } from "@/components/Icon";
 import { LeftNav } from "@/components/LeftNav";
 import { LeftNavLink } from "@/components/LeftNavLink";
 import { DefaultNav } from "@/components/layout/DefaultNav";
 import { useWorkspaces } from "@/graphql/queries/useWorkspaces";
 import { Workspace } from "@/graphql/types";
+import useTooltip from "@/hooks/useTooltip";
 import { usePathname } from "next/navigation";
 
 export function TeamNav() {
   const { workspaces } = useWorkspaces();
 
   let workspace: Workspace | undefined = undefined;
+
+  const { onMouseEnter, onMouseMove, onMouseLeave, tooltip } = useTooltip({
+    tooltip: "Tests view is temporarily disabled while we fix an performance issue.",
+  });
 
   const pathname = usePathname();
   if (pathname && pathname.startsWith("/team/")) {
@@ -37,12 +43,23 @@ export function TeamNav() {
             isActive={!!pathname?.endsWith("runs")}
             label="Runs"
           />
-          <LeftNavLink
+          {/* <LeftNavLink
             href={`/team/${workspace.id}/tests`}
             iconType="menu-tests"
             isActive={!!pathname?.endsWith("tests")}
             label="Tests"
-          />
+          /> */}
+          <div
+            className={`flex flex-row gap-2 items-center text-white px-2 py-1 transition rounded text-gray-400`}
+            data-test-name="LeftNavLink"
+            onMouseEnter={onMouseEnter}
+            onMouseMove={onMouseMove}
+            onMouseLeave={onMouseLeave}
+          >
+            <Icon className="h-4 w-4 hidden md:block shrink-0" type="menu-tests" />
+            <div className="grow truncate">Tests</div>
+          </div>
+          {tooltip}
         </>
       ) : (
         <LeftNavLink
