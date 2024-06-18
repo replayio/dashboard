b9afa3026a7ce38c59c354a79e69aedab12138e5
Jon Bell <github@lot23.com>
2024-06-19 08:18:14 +1200

    Remove debug=true

--- src/utils/mixpanel.ts
+++ src/utils/mixpanel.ts
@@ -2,7 +2,6 @@ import mixpanel from "mixpanel-browser";
 
 export const initializeMixPanel = () => {
   mixpanel.init("ffaeda9ef8fb976a520ca3a65bba5014", {
-    debug: true,
     track_pageview: true,
     persistence: "localStorage",
   });
