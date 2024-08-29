5f1d3a8ed7a202644e522ec053a7e49d0ec2ee5b
Dan Miller <dan@dmiller.dev>
2024-08-29 10:42:24 -0700

    import dynamic performance mockup (#183)

---
+++ src/components/performance/RecordingDisplay.tsx
@@ -0,0 +1,25 @@
+import { RecordingLink } from "./RecordingLink";
+
+// Displays overall information about the recording being examined.
+
+interface RecordingDisplayProps {
+  recordingURL: string;
+}
+
+export function RecordingDisplay(props: RecordingDisplayProps) {
+  const { recordingURL } = props;
+
+  const url = new URL(recordingURL);
+  const name = url.host;
+
+  return (
+    <div className="RecordingInfo">
+      <RecordingLink
+        className="DefaultRecordingLink"
+        text={`Recording of ${name}`}
+        point={undefined}
+        time={undefined}
+      ></RecordingLink>
+    </div>
+  );
+}
