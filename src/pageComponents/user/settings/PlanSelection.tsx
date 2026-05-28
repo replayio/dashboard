f6fe2f27183a936a6d67cfd5d189a9c1e7e214f3
Strider Wilson <strider.wilson@replay.io>
2026-05-28 17:53:13 -0400

    Change redirect for Contact button

--- src/pageComponents/user/settings/PlanSelection.tsx
+++ src/pageComponents/user/settings/PlanSelection.tsx
@@ -119,7 +119,7 @@ function PlanCard({ plan, isCurrentPlan }: { plan: StripePlan; isCurrentPlan: bo
 
       <div className="mt-auto pt-2">
         {plan.tier === "enterprise" ? (
-          <a href="mailto:sales@replay.io" className="block">
+          <a href="https://www.replay.io/contact" target="_blank" rel="noopener noreferrer" className="block">
             <Button className="w-full" variant="outline">
               Contact Sales
             </Button>
